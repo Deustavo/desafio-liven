258716ebbeea49eccd8d627ccb8662e822383bcf
gustavo.rodrigues <gustavo.rodrigues@hubess.com.br>
2021-11-22 17:28:31 -0300

    iniciando projeto

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -2,8 +2,9 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return <>
-    <section className="app__home padding-page">
-      <h2>Produtos</h2>
+    <section className="app__header padding-page">
+      <h2>Loja Simples</h2>
+      <p>Carrinho</p>
     </section>
   </>
 }
